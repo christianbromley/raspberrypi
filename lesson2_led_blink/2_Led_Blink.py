06545ad498e299b93b2d3b9a213e8d7115cefed5
Christian Bromley <cbromley66@googlemail.com>
2022-10-09 20:51:51 +0100

    setting max on number of blinks using while loop

--- lesson2_led_blink/2_Led_Blink.py
+++ lesson2_led_blink/2_Led_Blink.py
@@ -7,12 +7,15 @@ GPIO.setmode(GPIO.BCM)        # use BCM numbers
 GPIO.setup(ledPin,GPIO.OUT)   #set the ledPin OUTPUT mode
 GPIO.output(ledPin,GPIO.LOW)  # make ledPin output LOW level
 
-while True:    #loop
+# initialise count
+n_blinks = 0
+while n_blinks < 10:    #loop
     GPIO.output(ledPin,GPIO.HIGH)  #turn on led
     print("turned on the led")  #Print in the terminal
-    time.sleep(1)              #wait for 1 second
+    time.sleep(5)              #wait for 1 second
     GPIO.output(ledPin,GPIO.LOW)   #turn off led
     print("turned off the led")
-    time.sleep(1)
+    time.sleep(2)
+    n_blinks += 1
 
 GPIO.cleanup()    #release all GPIO
